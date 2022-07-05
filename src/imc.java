ce949a37ff770b4dcb5e4f79247ad4f9b05e458a
IsmaelB13 <ismaelballesteros9@gmail.com>
2022-07-04 23:29:30 -0500

    Cambios realizados con exito!!

--- src/imc.java
+++ src/imc.java
@@ -4,10 +4,11 @@ public class imc {
     public static void main(String[] args) {
         //JOptionPane.showMessageDialog(null, "bienvenidos");
         //JOptionPane.showInputDialog(null, "introduzca un dato: ")
-        int altura=Integer.parseInt(JOptionPane.showInputDialog(null, "introduzca su altura en centimetros: ", "Sumando", JOptionPane.QUESTION_MESSAGE));
-        int peso=Integer.parseInt(JOptionPane.showInputDialog(null, "introduzca su peso en kilogramos: ", "Sumando", JOptionPane.QUESTION_MESSAGE));
-        float total= (float) (altura/Math.pow(peso,2));
-        JOptionPane.showMessageDialog(null, "Si IMC es: "+total, "sumando", JOptionPane.INFORMATION_MESSAGE);
+        //Pedimos los el peso y talla al usuario
+        int altura1=Integer.parseInt(JOptionPane.showInputDialog(null, "introduzca su altura en centimetros: ", "Sumando", JOptionPane.QUESTION_MESSAGE));
+        int peso1=Integer.parseInt(JOptionPane.showInputDialog(null, "introduzca su peso en kilogramos: ", "Sumando", JOptionPane.QUESTION_MESSAGE));
+        float totalImc= (float) (altura1/Math.pow(peso1,2));//COn esta formula calculamos el IMC de una persona
+        JOptionPane.showMessageDialog(null, "Si IMC es: "+totalImc, "sumando", JOptionPane.INFORMATION_MESSAGE);
         //System.out.println(numero1+numero2);
     }
 
